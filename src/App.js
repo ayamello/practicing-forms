cfb22b0e0cf321e4c08888ab06239e5dfe3c9e79
Ayana Mello <ayanamello27@gmail.com>
2021-07-14 20:31:12 -0300

    Add routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import Register from './pages/Register';
+import RegisterCompleted from './pages/RegisterCompleted';
+
+const { Switch, Route } = require("react-router-dom");
+
 function App() {
     return(
-        <div></div>
+        <div>
+            <Switch>
+                <Route path="/cadastro">
+                    <Register />
+                </Route>
+                <Route path="/cadastro-concluido">
+                    <RegisterCompleted />
+                </Route>
+            </Switch>
+        </div>
     );
 }
 
